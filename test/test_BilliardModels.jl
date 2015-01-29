1f19a1d7cb697a9c74b9d757b2811070fc37d339
David P. Sanders <dpsanders@gmail.com>
2015-01-29 19:20:27 +0300

    Tests pass including BilliardModel tests

--- test/test_BilliardModels.jl
+++ test/test_BilliardModels.jl
@@ -7,20 +7,21 @@ using Vector2d
 
 facts("Particle tests") do
 
-    p = Particle([1, 2], [3, 4])
-    @fact p => Particle(Vector2D{Int64}(1,2), Vector2D{Int64}(3,4))
+    p = Particle([1, 2.], [3., 4.5])
+    @fact p.x => Vector2D{Float64}(1,2)
+    @fact p.v => Vector2D{Float64}(3.0,4.5)
 
 end
 
 
 facts("Disc tests") do
     d = Disc([1, 2], 3)
-    @fact d => Disc(Vector2D{Int64}(1,2),3.0)
+    @fact d.radius => 3.0
 
     d = Disc([0, 0], 1)
     p = Particle([-2, 0], [1, 0])
 
-    @fact collision_time(p, d)
+    @fact collision_time(p, d) => 1
 
 end
 
