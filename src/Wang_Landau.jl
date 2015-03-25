16b5ab93cf7a6e427964e40f0e01ed9c417b8ccc
David P. Sanders <dpsanders@gmail.com>
2015-03-25 13:00:18 -0600

    Added free_path_distribution to explore phase space. Rewrote to use proposal and acceptance. Started writing WL but seems to be more complicated to implement

--- src/Wang_Landau.jl
+++ src/Wang_Landau.jl
@@ -13,6 +13,28 @@ end
 
 unif_rand(delta) = delta*(rand()-0.5)
 
+function propose(x, y, theta, radius, delta)
+    x_new = x + unif_rand(delta)
+    y_new = y + unif_rand(delta)
+
+     # check outside disc:
+    if x*x + y*y < radius^2
+        x_new, y_new = x, y   # return to where it was
+    else
+        # periodize:
+        (x_new > 0.5) && (x_new -= 1)
+        (x_new < -0.5) && (x_new += 1)
+        (y > 0.5) && (y -= 1)
+        (y < -0.5) && (y += 1)
+    end
+
+    theta += unif_rand(delta)  # in any case change theta
+
+    x, y, theta
+end
+
+acceptance_prob(x, y, theta) = 1.0  # standard random walk
+
 function explore_phase_space(radius=0.1, delta=0.1, num_steps=1000)
 
     billiard_table = Sinai_billiard(radius, true, true)  # periodic in x and y
@@ -26,27 +48,19 @@ function explore_phase_space(radius=0.1, delta=0.1, num_steps=1000)
     thetas = [theta]
     free_paths = [find_free_path(billiard_table, xx, vv)]
 
+    num_free_path_bins = 1000
+    free_path_distribution = zeros(num_free_path_bins)
+
     for i in 1:num_steps
         x, y = xx[1], xx[2]
 
-        x += unif_rand(delta)
-        y += unif_rand(delta)
+        x_new, y_new, theta_new = propose(x, y, theta, radius, delta)
 
-        # periodize:
-        (x > 0.5) && (x -= 1)
-        (x < -0.5) && (x += 1)
-        (y > 0.5) && (y -= 1)
-        (y < -0.5) && (y += 1)
-
-        # check outside disc:
-        if x*x + y*y < radius^2
-            continue
+        if rand() < acceptance_prob(x, y, theta)
+            x, y, theta = x_new, y_new, theta_new
         end
 
         xx_new = Vector2D(x, y)
-
-        theta += unif_rand(delta)
-
         vv = Vector2D(cos(theta), sin(theta))
 
 
@@ -57,11 +71,16 @@ function explore_phase_space(radius=0.1, delta=0.1, num_steps=1000)
         push!(thetas, theta)
         push!(free_paths, free_path)
 
+        free_path_bin = ceil(free_path)
+        (free_path_bin > num_free_path_bins) && (free_path_bin = num_free_path_bins)
+
+        free_path_distribution[free_path_bin] += 1
+
         xx = xx_new
 
     end
 
-    xs, ys, thetas, free_paths
+    xs, ys, thetas, free_paths, free_path_distribution
 end
 
 
