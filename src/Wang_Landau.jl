7a6266738b8bf9fbc5b2f7f1f925f33b048e6c3a
David P. Sanders <dpsanders@gmail.com>
2015-06-01 15:23:37 +0200

    Non-uniform sampling

--- src/Wang_Landau.jl
+++ src/Wang_Landau.jl
@@ -2,7 +2,7 @@
 
 using BilliardModels
 
-function find_free_path(billiard_table, x, v)
+function first_free_path(billiard_table, x, v)
     l = Vector2D(0, 0)
     p = ParticleOnLattice(x, v, l)
     num_collisions = 1
@@ -11,11 +11,14 @@ function find_free_path(billiard_table, x, v)
     free_paths[1]
 end
 
-unif_rand(delta) = delta*(rand()-0.5)
 
-function propose(x, y, theta, radius, delta)
-    x_new = x + unif_rand(delta)
-    y_new = y + unif_rand(delta)
+unif_rand(δ) = δ*(rand()-0.5)  # uniform real ∈ [-δ, δ]
+
+
+
+function propose(x, y, θ, radius, δ)  # random walk proposal
+    x_new = x + unif_rand(δ)
+    y_new = y + unif_rand(δ)
 
      # check outside disc:
     if x*x + y*y < radius^2
@@ -28,24 +31,25 @@ function propose(x, y, theta, radius, delta)
         (y < -0.5) && (y += 1)
     end
 
-    theta += unif_rand(delta)  # in any case change theta
+    θ += unif_rand(δ)  # in any case change θ
 
-    x, y, theta
+    x, y, θ
 end
 
-acceptance_prob(x, y, theta) = 1.0  # standard random walk
+acceptance_prob(x, y, θ) = 1.0  # standard random walk
 
-function explore_phase_space(radius=0.1, delta=0.1, num_steps=1000)
+function explore_phase_space(radius=0.1, δ=0.1, num_steps=1000)
 
     billiard_table = Sinai_billiard(radius, true, true)  # periodic in x and y
 
     xx, vv = initial_condition(billiard_table, -.5, .5, -.5, .5)
 
-    theta = atan2(vv[2], vv[1])  # this is how I generated the velocity to start with (from an angle) -- a bit ridiculous
+    θ = atan2(vv[2], vv[1])
+    # this is how I generated the velocity to start with (from an angle) -- it's a bit ridiculous to undo that
 
     xs = [xx[1]]
     ys = [xx[2]]
-    thetas = [theta]
+    θs = [θ]
     free_paths = [find_free_path(billiard_table, xx, vv)]
 
     num_free_path_bins = 1000
@@ -54,21 +58,21 @@ function explore_phase_space(radius=0.1, delta=0.1, num_steps=1000)
     for i in 1:num_steps
         x, y = xx[1], xx[2]
 
-        x_new, y_new, theta_new = propose(x, y, theta, radius, delta)
+        x_new, y_new, θ_new = propose(x, y, θ, radius, δ)
 
-        if rand() < acceptance_prob(x, y, theta)
-            x, y, theta = x_new, y_new, theta_new
+        if rand() < acceptance_prob(x, y, θ)
+            x, y, θ = x_new, y_new, θ_new
         end
 
         xx_new = Vector2D(x, y)
-        vv = Vector2D(cos(theta), sin(theta))
+        vv = Vector2D(cos(θ), sin(θ))
 
 
         free_path = find_free_path(billiard_table, xx_new, vv)
 
         push!(xs, x)
         push!(ys, y)
-        push!(thetas, theta)
+        push!(θs, θ)
         push!(free_paths, free_path)
 
         free_path_bin = ceil(free_path)
@@ -80,7 +84,7 @@ function explore_phase_space(radius=0.1, delta=0.1, num_steps=1000)
 
     end
 
-    xs, ys, thetas, free_paths, free_path_distribution
+    xs, ys, θs, free_paths, free_path_distribution
 end
 
 
